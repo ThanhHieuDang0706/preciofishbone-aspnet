d8b6c669783f72c67cccee1742d8e2ea0a226e17
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-02-17 17:11:26 +0700

    add file mapper

---
+++ PrecioFishBoneVietnamASP.NETTraining/Profiles/FileProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrecioFishboneVietnamASP.NETTraining.Entities;
+using PrecioFishboneVietnamASP.NETTraining.Models;
+
+namespace PrecioFishboneVietnamASP.NETTraining.Profiles
+{
+    public class FileProfiles : Profile
+    {
+        public FileProfiles ()
+        {
+            CreateMap<MyFile, FileDto>();
+        }
+    }
+}
--- PrecioFishBoneVietnamASP.NETTraining/Program.cs
+++ PrecioFishBoneVietnamASP.NETTraining/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 builder.Services.AddDbContext<ItemContext>
