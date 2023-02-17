0103aa24028c116f9131fff1510ab0cf669da687
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-02-17 16:36:30 +0700

    add empty controller (for later use)

---
+++ PrecioFishBoneVietnamASP.NETTraining/Controllers/FilesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PrecioFishboneVietnamASP.NETTraining.Controllers
+{
+    [Route("api/folders/{folderId}/files")]
+    [ApiController]
+    public class Files : ControllerBase
+    {
+        
+    }
+}
