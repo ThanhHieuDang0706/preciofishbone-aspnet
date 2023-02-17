fdf0c8a00ce1c727621d4997b89061f3acd05905
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-02-17 17:11:37 +0700

    add get files

--- PrecioFishBoneVietnamASP.NETTraining/Controllers/FilesController.cs
+++ PrecioFishBoneVietnamASP.NETTraining/Controllers/FilesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PrecioFishboneVietnamASP.NETTraining.Models;
+using PrecioFishboneVietnamASP.NETTraining.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +11,26 @@ namespace PrecioFishboneVietnamASP.NETTraining.Controllers
     [ApiController]
     public class Files : ControllerBase
     {
-        
+        private readonly IItemRepository _itemRepository;
+        private readonly IMapper _mapper;
+        public Files(IItemRepository itemRepository, IMapper mapper)
+        {
+            _itemRepository = itemRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FileDto>>> GetFiles(int folderId)
+        {
+            try
+            {
+                var fileEntities = await _itemRepository.GetFilesInFolder(folderId);
+                return Ok(_mapper.Map<IEnumerable<FileDto>>(fileEntities));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
