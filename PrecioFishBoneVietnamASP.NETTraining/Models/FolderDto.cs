6bf6a706e0fdf4986a46d98cfa797730ebb66049
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-02-17 17:22:00 +0700

    create mapper and update folder dto

--- PrecioFishBoneVietnamASP.NETTraining/Models/FolderDto.cs
+++ PrecioFishBoneVietnamASP.NETTraining/Models/FolderDto.cs
@@ -1,4 +1,6 @@
-﻿namespace PrecioFishboneVietnamASP.NETTraining.Models
+﻿using PrecioFishboneVietnamASP.NETTraining.Entities;
+
+namespace PrecioFishboneVietnamASP.NETTraining.Models
 {
     public class FolderDto
     {
@@ -7,7 +9,11 @@
         public DateTime Modified { get; set; }
         public string ModifiedBy { get; set; } = null!;
         public Entities.Type ItemType { get; set; } = Entities.Type.Folder;
-        
+
+        public ICollection<Folder> Folders { get; set; } = new List<Folder>();
+
+        public ICollection<MyFile> Files { get; set; } = new List<MyFile>();
+
         public int? ParentFolderId { get; set; }
     }
 }
---
+++ PrecioFishBoneVietnamASP.NETTraining/Profiles/FolderProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrecioFishboneVietnamASP.NETTraining.Entities;
+using PrecioFishboneVietnamASP.NETTraining.Models;
+
+namespace PrecioFishboneVietnamASP.NETTraining.Profiles
+{
+    public class FolderProfiles : Profile
+    {
+        public FolderProfiles()
+        {
+            CreateMap<Folder, FolderDto>();
+        }
+    }
+}
