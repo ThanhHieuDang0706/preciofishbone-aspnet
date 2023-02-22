449fc3e64de66425455b98d4882785b2246a78bd
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-02-22 17:59:57 +0700

    update client code add role based

--- PrecioFishBoneVietnamASP.NETTraining/wwwroot/src/scripts/pages/home-page.ts
+++ PrecioFishBoneVietnamASP.NETTraining/wwwroot/src/scripts/pages/home-page.ts
@@ -3,7 +3,8 @@ import { renderTable } from '../components/_table';
 import renderModalForm from '../components/_modal';
 import HomeState from '../types/_homepage';
 import { ROOT_FOLDER_ID } from '../utilities/_folder';
-import { addSignInButtonEventClick, getTokenRedirect, handleRedirectPromise, selectAccount } from '../auth/_authRedirect';
+import { addSignInButtonEventClick, handleRedirectPromise, selectAccount } from '../auth/_authRedirect';
+import { renderEmptyAlert } from '../components/_alert';
 
 export const homeState: HomeState = {
   currentFolderId: ROOT_FOLDER_ID,
@@ -19,6 +20,7 @@ ready(async () => {
 
   // render intial view
   renderModalForm(homeState);
+  renderEmptyAlert();
   await renderTable(homeState);
 
   // $('#back-button').on('click', () => {
